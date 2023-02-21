f5f791686ad380298e8aeec2fec108c6400aa26b
mrsklg <100710286+mrsklg@users.noreply.github.com>
2023-02-21 15:45:59 +0100

    Uploading random password generator

---
+++ index.js
@@ -0,0 +1,23 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let firstPassword = document.getElementById("first-password")
+let secondPassword = document.getElementById("second-password")
+const passwordLength = document.getElementById("passwd-length")
+
+
+
+function getRandomPassword() {
+    let password = ""
+    for (let i = 0; i < passwordLength.value; i++)
+    {
+        password += characters[Math.floor(Math.random() * characters.length)]
+    }
+    return password
+}
+
+function generatePasswords() {
+    firstPassword.textContent = getRandomPassword()
+    secondPassword.textContent = getRandomPassword()
+    passwordLength.value = ""
+}
